89f1a64f5ade94e1e10de1456cdf75bf463839e0
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-04-19 16:58:25 -0500

    Add register and login routes

--- main.py
+++ main.py
@@ -65,6 +65,16 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 @app.route('/create-project', methods=["GET", "POST"])
 def create_project():
     form = CreateProjectForm()
