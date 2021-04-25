e17cf460e65d40d535629d7dfc3dadcf70c926a1
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-04-25 00:39:34 -0500

    Change database uri

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 bootstrap = Bootstrap(app)
 ckeditor = CKEditor(app)
 
-uri = os.getenv(
-    "DATABASE_URL", "sqlite:///devtick.db")
+uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
