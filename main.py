0b0ca69699c0b5687f147b218411573ef1b5f164
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-05-19 21:41:48 -0500

    Configure app outside of function

--- main.py
+++ main.py
@@ -91,49 +91,43 @@ def is_viewer(project_id):
         return association.user_role == "Viewer"
 
 
+# Creates Flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 
+app.register_blueprint(general_blueprint)
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(projects_blueprint)
 
-def configure_app():
-    # Creates Flask app
-    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
+# Configures Bootstrap and CKEditor for the app.
+bootstrap = Bootstrap(app)
+ckeditor = CKEditor(app)
+talisman = Talisman(app, content_security_policy=csp)
 
-    app.register_blueprint(general_blueprint)
-    app.register_blueprint(auth_blueprint)
-    app.register_blueprint(projects_blueprint)
+# Configures the database
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-    # Configures Bootstrap and CKEditor for the app.
-    bootstrap = Bootstrap(app)
-    ckeditor = CKEditor(app)
-    talisman = Talisman(app, content_security_policy=csp)
+# Initializes the login manager for the app
+login_manager.init_app(app)
 
-    # Configures the database
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
+# Adds views to the admin panel
+admin = Admin(app, name='DevTickAdmin', index_view=AdminIndexView())
+admin.add_view(DevTickModelView(User, db.session))
+admin.add_view(DevTickModelView(Project, db.session))
+admin.add_view(DevTickModelView(AssociatedUser, db.session))
+admin.add_view(DevTickModelView(Ticket, db.session))
 
-    # Initializes the login manager for the app
-    login_manager.init_app(app)
-
-    # Adds views to the admin panel
-    admin = Admin(app, name='DevTickAdmin', index_view=AdminIndexView())
-    admin.add_view(DevTickModelView(User, db.session))
-    admin.add_view(DevTickModelView(Project, db.session))
-    admin.add_view(DevTickModelView(AssociatedUser, db.session))
-    admin.add_view(DevTickModelView(Ticket, db.session))
-
-    # Sets jinja globals for the user permission helper functions so they can be called from templates
-    app.jinja_env.globals.update(is_creator=is_creator)
-    app.jinja_env.globals.update(is_admin=is_admin)
-    app.jinja_env.globals.update(is_editor=is_editor)
-    app.jinja_env.globals.update(is_viewer=is_viewer)
-
-    # Runs the app
-    app.run(debug=True)
+# Sets jinja globals for the user permission helper functions so they can be called from templates
+app.jinja_env.globals.update(is_creator=is_creator)
+app.jinja_env.globals.update(is_admin=is_admin)
+app.jinja_env.globals.update(is_editor=is_editor)
+app.jinja_env.globals.update(is_viewer=is_viewer)
 
 
 # Runs the app
 if __name__ == "__main__":
-    configure_app()
+    app.run()
